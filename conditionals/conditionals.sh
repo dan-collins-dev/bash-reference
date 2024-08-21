e684847646effdef211d86865e012fc272ca0b14
Dan Collins <dan.collins.dev@gmail.com>
2024-08-21 10:53:32 -0400

    added switch statement example to conditionals script

--- conditionals/conditionals.sh
+++ conditionals/conditionals.sh
@@ -10,3 +10,28 @@ then
 else
     echo "No, your name is "${my_name}
 fi
+
+# Switch/case statements
+# case matters
+captains=("Sisko" "Archer" "Janeway" "Picard" "Kirk")
+
+read -p "Who's your favorite Star Trek Captain? " choice
+
+case $choice in
+
+    Sisko)
+        echo -n "${choice} is the best captain."
+        ;;
+    
+    Archer | Janeway | Picard)
+        echo -n "${choice} is a pretty good captain."
+        ;;
+    
+    Kirk)
+        echo -n "SHAME ON YOU!"
+        ;;
+    
+    *)
+        echo -n "${choice} is not a captain."
+        ;;
+esac
