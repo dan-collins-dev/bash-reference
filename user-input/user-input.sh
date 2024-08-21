97df99313eb2a1ccd462d18cc822534039881bd2
dan <dan.collins.dev@gmail.com>
2024-08-20 22:10:51 -0400

    added a section for user input/prompts

---
+++ user-input/user-input.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Prompts the user for input
+echo "Who is the Batman?"
+read name
+
+# Response using input
+echo "I don't think $name is Batman."
+
+# Echo/Prompt shorthand is `read -p "Who is the Batman? " name`
+read -p "Who is the best Robin? " name
+echo "I don't think $name is the best Robin."
